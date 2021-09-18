d85da04729af8af19b046a43faa9d465693cff6e
Sean Bourg <sean.bourg@gmail.com>
2021-09-18 09:29:43 -0500

    Updated setup script to check for folder & key prior to building

--- sftp/bin/setup.sh
+++ sftp/bin/setup.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 # Set up local folder structure and create the required key files for docker SFTP image.
 
-# Create folders
-mkdir private
-mkdir sftp
-mkdir sftp/.ssh
+if [ ! -d sftp ]
+then
+    mkdir sftp
+    mkdir sftp/.ssh
 
-# Create authorized keys file
-touch home/.ssh/authorized_keys
+    # Create authorized keys file
+    touch sftp/.ssh/authorized_keys
+fi
+
+# Create folders
+[ ! -d private ] && mkdir private
 
 #  Generate server keys
-ssh-keygen -t ed25519 -f private/ssh_host-ed25519_key < /dev/null
-ssh-keygen -t rsa -b 4096 -f private/ssh_host-rsa_key < /dev/null
+[ ! -f private/ssh_host-ed25519_key ]  && ssh-keygen -t ed25519 -f private/ssh_host-ed25519_key < /dev/null
+[ ! -f private/ssh_host-rsa_key ] && ssh-keygen -t rsa -b 4096 -f private/ssh_host-rsa_key < /dev/null
